e9a9dfe24fd5be1b643fe8a21cc6b643cc20faa7
Steven Hollingsworth <shollingsworth@barracuda.com>
2021-07-08 12:01:33 -0700

    Parameterized the password for run.sh

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ```
 cd arch/
 ./build.sh
-./run.sh
+./run.sh "mypassword"
 # connect with vnc client of choice
 krdc vnc://localhost:5900
 ```
--- arch/run.sh
+++ arch/run.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+password="${1?"argv0:password"}"
+
 docker run \
     -p 5900:5900 \
-    -e VNC_PASSWORD=testing \
+    -e VNC_PASSWORD="${password}" \
     --privileged \
     -it --rm \
     --name arch \
